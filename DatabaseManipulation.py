88557f71a80c049e07ae9e5123ef8cf409db9b86
ElDiablo247 <raul_birta10@yahoo.com>
2024-12-06 20:10:47 +0100

    Added attribute unique_codes of type set() to list the unique codes already created so the new generated codes can look in the set for already existing codes to not generate the same ones. positions table in the database now has column sector to store the sector aswell. if asset is not a stock then the value is NULL. Open position function now is divided to other functions for example if a position is of type stock the open_stock_position is called that takes care of the change in total_amount column OR adds new stock if does not exist. Same happens for etfs and crypto. generate_unique_id now checks self.unique_codes to see if the generated code may have already been created and if yes then generates another code so that all codes are unique.

--- DatabaseManipulation.py
+++ DatabaseManipulation.py
@@ -13,6 +13,7 @@ class DatabaseManipulation:
         self.etfs_df = pd.DataFrame()
         self.crypto_df = pd.DataFrame()
         self.positions_df = pd.DataFrame()
+        self.unique_codes = set()
         self.sql_to_df()
 
     def execute_many(self, query, params_list):
@@ -46,7 +47,8 @@ class DatabaseManipulation:
                 position_id INT PRIMARY KEY,
                 position_name VARCHAR(255) NOT NULL,
                 position_amount INT NOT NULL,
-                asset_type VARCHAR(255) NOT NULL
+                asset_type VARCHAR(255) NOT NULL,
+                sector VARCHAR(255) NOT NULL
             );"""
         ]
         for query in queries:
@@ -59,48 +61,60 @@ class DatabaseManipulation:
             self.etfs_df = pd.read_sql_table('etfs', connection)
             self.crypto_df = pd.read_sql_table('crypto', connection)
             self.positions_df = pd.read_sql_table('positions', connection)
-            print(self.stocks_df)
-            print("\n")
-            print(self.etfs_df)
-            print("\n")
-            print(self.crypto_df)
-            print("\n")
-            print(self.positions_df)
-            print("\n")
+            self.show_tables()
+            self.unique_codes = set(self.positions_df['position_id'])
 
     def open_position(self, asset_name: str, amount: int, asset_type: str):
+        sector = None
+        if asset_type.lower() == 'stock':
+            sector = input("This position is for a stock so a sector must be entered(Technology, Finance etc.): ")
+            self.add_stock_position(asset_name, amount, sector)
+        
+        if asset_type.lower() == 'etf':
+            self.add_etf_position(asset_name, amount)
+        
+        if asset_type.lower() == 'crypto':
+            self.add_crypto_position(asset_name, amount)
+
+        #a unique id is generated to represent each new position
         unique_id = self.generate_unique_id()
-        new_entry = {'position_id': unique_id, 'position_name': asset_name, 'position_amount': amount, 'asset_type': asset_type}
+        new_entry = {'position_id': unique_id, 'position_name': asset_name, 'position_amount': amount, 'asset_type': asset_type, 'sector': sector}
         self.positions_df = pd.concat([self.positions_df, pd.DataFrame([new_entry])], ignore_index=True)
         
-        if asset_type.lower() == 'stock':
-            if asset_name in self.stocks_df['stock_name'].values:
-                self.stocks_df.loc[self.stocks_df['stock_name'] == asset_name, 'total_amount'] += amount
-            else:
-                sector = input("This position is for a stock so a sector must be entered(Technology, Finance etc.): ")
-                new_entry = {'stock_name': asset_name, 'total_amount': amount, 'sector': sector}
-                self.stocks_df = pd.concat([self.stocks_df, pd.DataFrame([new_entry])], ignore_index=True)
+    def add_stock_position(self, stock_name: str, amount: int, stock_sector: str):     
+        if stock_name in self.stocks_df['stock_name'].values:
+            self.stocks_df.loc[self.stocks_df['stock_name'] == stock_name, 'total_amount'] += amount
+        else:
+            new_entry = {'stock_name': stock_name, 'total_amount': amount, 'sector': stock_sector}
+            self.stocks_df = pd.concat([self.stocks_df, pd.DataFrame([new_entry])], ignore_index=True)
 
-        if asset_type.lower() == 'etf':
-            if asset_name in self.etfs_df['etf_name'].values:
-                self.etfs_df.loc[self.etfs_df['etf_name'] == asset_name, 'total_amount'] += amount
-            else:
-                new_entry = {'etf_name': asset_name, 'total_amount': amount}
-                self.etfs_df = pd.concat([self.etfs_df, pd.DataFrame([new_entry])], ignore_index=True)
+    def add_etf_position(self, etf_name: str, amount: int):
+        if etf_name in self.etfs_df['etf_name'].values:
+            self.etfs_df.loc[self.etfs_df['etf_name'] == etf_name, 'total_amount'] += amount
+        else:
+            new_entry = {'etf_name': etf_name, 'total_amount': amount}
+            self.etfs_df = pd.concat([self.etfs_df, pd.DataFrame([new_entry])], ignore_index=True)
 
 
-        if asset_type.lower() == 'crypto':
-            if asset_name in self.crypto_df['crypto_name'].values:
-                self.crypto_df.loc[self.crypto_df['crypto_name'] == asset_name, 'total_amount'] += amount
-            else:
-                new_entry = {'crypto_name': asset_name, 'total_amount': amount}
-                self.crypto_df = pd.concat([self.crypto_df, pd.DataFrame([new_entry])], ignore_index=True)
+    def add_crypto_position(self, crypto_name: str, amount: int):
+        if crypto_name in self.crypto_df['crypto_name'].values:
+            self.crypto_df.loc[self.crypto_df['crypto_name'] == crypto_name, 'total_amount'] += amount
+        else:
+            new_entry = {'crypto_name': crypto_name, 'total_amount': amount}
+            self.crypto_df = pd.concat([self.crypto_df, pd.DataFrame([new_entry])], ignore_index=True)
+
+        
 
     def generate_unique_id(self):
-        letter = random.choice(string.ascii_uppercase)
-        numbers = f"{random.randint(0, 999):03d}"
-        unique_id = f"{letter}{numbers}"
-        return unique_id
+        flag = False
+        while flag == False:
+            letter = random.choice(string.ascii_uppercase)
+            numbers = f"{random.randint(0, 999):03d}"
+            unique_id = f"{letter}{numbers}"
+            if unique_id not in self.unique_codes:
+                self.unique_codes.add(unique_id)
+                flag = True
+                return unique_id
     
     def show_tables(self):
         print(self.stocks_df)
@@ -116,5 +130,3 @@ class DatabaseManipulation:
 
             
 master = DatabaseManipulation()
-
-
