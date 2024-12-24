bab7c293f027092395a39bb1bf34b8524004bde1
ElDiablo247 <raul_birta10@yahoo.com>
2024-12-24 23:46:38 +0100

    testing.py file was created to be used for testing only. New function show_data_chart creates a barplot to show the user a visual representation of data. (for that matplotlib.pyplot was imported)

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,5 +1,6 @@
 from DatabaseManipulation import DatabaseManipulation
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 
@@ -24,7 +25,7 @@ class DataAnalysis:
         total_amount_all_assets = self.positions_df['position_amount'].sum()
 
         # amount percentages for each asset type in comparison to total invested
-        stocks_amount_percentage = (total_amount_stocks / total_amount_all_assets) * 100
+        stocks_amount_percentage = (total_amount_stocks / total_amount_all_assets) * 100 
         etfs_amount_percentage = (total_amount_etfs / total_amount_all_assets) * 100
         crypto_amount_percentage = (total_amount_crypto / total_amount_all_assets) * 100
         all_assets_amount_percentage = (total_amount_all_assets / total_amount_all_assets) * 100
@@ -40,6 +41,26 @@ class DataAnalysis:
     def show_data_analysis_df(self):
         print(self.data_analysis_df)
 
+    def show_data_chart(self):
+        plt.figure(figsize=(8, 6))
+        sns.barplot(
+            x='asset type', 
+            y='amount invested(percentage)', 
+            data=self.data_analysis_df, 
+            palette='Blues_r'
+        )
+        for index, row in self.data_analysis_df.iterrows():
+            plt.text(index, row['amount invested(percentage)'], f"${row['total amount invested']}", ha='center')
+
+        # Add titles and labels
+        plt.title("Portfolio Allocation")
+        plt.ylabel("Percentage of Total Investment")
+        plt.xlabel("Asset Type")
+        plt.show()
+
+
+
 master = DatabaseManipulation()
 analysis = DataAnalysis(master)
 analysis.show_data_analysis_df()
+analysis.show_data_chart()
