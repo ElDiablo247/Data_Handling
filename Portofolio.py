3cdc2a72981647c6f455dab8a89686ff55d868ed
ElDiablo247 <raul_birta10@yahoo.com>
2025-08-09 22:29:49 +0200

    Implement user registration and login functionality with secure password handling and database integration

--- Portofolio.py
+++ Portofolio.py
@@ -1,4 +1,5 @@
- from sqlalchemy import create_engine, text
+
+from sqlalchemy import create_engine, text
 import pandas as pd
 import random
 import string
@@ -26,60 +27,9 @@ class Portofolio:
     def get_account_amount(self) -> int:
         return self.account_amount
 
-    def set_username(self, username: str):
-        if not isinstance(username, str):
-            raise ValueError("Name must be a string")
-        if self.is_valid_username(username):
-            self.username = username
-        else:
-            raise ValueError("Username does not comply to the username rules. Only a-z, A-Z, and 0-9 characters are allowed.")
-
     def get_username(self) -> str:
         return self.username
     
-    @staticmethod
-    def is_valid_username(username: str) -> bool:
-        """Ensure username contains only letters, numbers, and underscores"""
-        return re.match(r"^[a-zA-Z0-9]+$", username) is not None
-
-    def execute_query(self, query: str, params=None, fetch=False):
-        """ Function that is called to execute an SQL query. It takes a string parameter which is a valid SQL query, and also a list of parameters if any. FOR EXAMPLE
-
-        query = '''
-        INSERT INTO stocks (stock_name, total_amount, sector)
-        VALUES (:name, :amount, :sector);
-        '''
-        params = {'name': 'AAPL', 'amount': 50, 'sector': 'Technology'}
-        master.execute_query(query, params)'''
-
-        query is the variable that contains the SQL query. Values are masked with (:) to avoid SQL injection. Params is a dictionary of key values to replace the masked values. At the end the query is executed.
-
-        If fetch is set to True then the value at the end is shown to the user.
-
-        Args:
-            query (str): A string SQL query
-            params (type depends, optional): Parameters could be anything but usually a string or integer. Defaults to None
-            fetch (bool, optional): If specifically set to True then the results are fetched and showed to the user. Defaults to False.
-
-        Returns:
-            _type_: Type depends on the type of the result (integer, string) and returns that value
-        """
-        with self.engine.begin() as connection:
-            result = connection.execute(text(query), params or {})
-            if fetch:
-                return result.fetchall()  # Fetch results if requested
-    
-    def execute_many(self, query: str, params_list: list):
-        """ Function that executes many queries at once. A query must be first created and then a list of values is fed to this function as a parameter. The function then iterates over each query and executes them.
-
-        Args:
-            query (str): An SQL string query
-            params_list (list): A list of parameters. For further information look into execute_query function documentation
-        """
-        with self.engine.connect() as connection:
-            with connection.begin():  # Automatically handles transactions
-                for params in params_list:
-                    connection.execute(text(query), params)
 
     def open_position(self, asset_name: str, amount: int, asset_type: str, asset_sector="-"):
         """This function opens a new asset position and stores it both in memory (via a pandas DataFrame) and in the SQL database.
@@ -117,59 +67,7 @@ class Portofolio:
         else:
             raise ValueError("Your account has ")
 
-    def add_stock_memory(self, stock_name: str, amount: int, stock_sector: str):
-        """ Function that adds a stock asset to memory dataframe. If the stock already exists in the `stocks_df` DataFrame, its `total_amount` is increased by the specified `amount`, and the change is reflected in the SQL database. If the stock does not exist, a new entry is created and added to both the in-memory DataFrame and the database.
-
-        Args:
-            stock_name (str): Name of the stock of type string
-            amount (int): The amount of the stock position
-            stock_sector (str): The sector in which the stock belongs (technology if stock is microsoft for example etc.)
-        """
-        if stock_name in self.stocks_df['stock_name'].values:
-            self.stocks_df.loc[self.stocks_df['stock_name'] == stock_name, 'total_amount'] += amount
-            print("Total amount of ", stock_name, " was increased by ", amount)
-            self.increase_asset_amount_db(stock_name, amount, 'stock')
-        else:
-            new_entry = {'stock_name': stock_name, 'total_amount': amount, 'sector': stock_sector}
-            self.stocks_df = pd.concat([self.stocks_df, pd.DataFrame([new_entry])], ignore_index=True)
-            print("New entry in stocks. ", stock_name, " with an amount of ", amount)     
-            self.insert_new_asset_db(stock_name, amount, 'stock', stock_sector)
-
-    def add_etf_memory(self, etf_name: str, amount: int):
-        """Function that adds an ETF asset to memory dataframe. If the ETF already exists in the `etfs_df` DataFrame, its `total_amount` is increased by the specified `amount`, and the change is reflected in the SQL database. If the ETF does not exist, a new entry is created and added to both the in-memory DataFrame and the database.
-
-        Args:
-            etf_name (str): Name of the ETF asset
-            amount (int): Amount of the ETF asset
-        """
-        if etf_name in self.etfs_df['etf_name'].values:
-            self.etfs_df.loc[self.etfs_df['etf_name'] == etf_name, 'total_amount'] += amount
-            print("Total amount of ", etf_name, " was increased by ", amount)          
-            self.increase_asset_amount_db(etf_name, amount, 'etf')
-        else:
-            new_entry = {'etf_name': etf_name, 'total_amount': amount}
-            self.etfs_df = pd.concat([self.etfs_df, pd.DataFrame([new_entry])], ignore_index=True)
-            print("New entry in ETFs. ", etf_name, " with an amount of ", amount)           
-            self.insert_new_asset_db(etf_name, amount, 'etf')
-
-    def add_crypto_memory(self, crypto_name: str, amount: int):
-        """Function that adds a crypto asset to the memory dataframe. If the cryptocurrency already exists in the `crypto_df` DataFrame, its `total_amount` is increased by the specified `amount`, and the change is reflected in the SQL database. If the cryptocurrency does not exist, a new entry is created and added to both the in-memory DataFrame and the database.
-
-        Args:
-            crypto_name (str): Name of the crypto asset.
-            amount (int): Amount of the crypto asset.
-        """
-        if crypto_name in self.crypto_df['crypto_name'].values:
-            self.crypto_df.loc[self.crypto_df['crypto_name'] == crypto_name, 'total_amount'] += amount
-            print("Total amount of ", crypto_name, " was increased by ", amount)
-            self.increase_asset_amount_db(crypto_name, amount, 'crypto')
-        else:
-            new_entry = {'crypto_name': crypto_name, 'total_amount': amount}
-            self.crypto_df = pd.concat([self.crypto_df, pd.DataFrame([new_entry])], ignore_index=True)
-            print("New entry in crypto. ", crypto_name, " with an amount of ", amount) 
-            self.insert_new_asset_db(crypto_name, amount, 'crypto')
-
-
+ 
     def close_position(self, position_id: str):
         """This function removes the specified position from the `positions_df` DataFrame in memory and the SQL database based on the user input unique position_id. It also adjusts the relevant asset amounts in their respective DataFrames (`stocks_df`, `etfs_df`, `crypto_df`) based on the asset type (stock, ETF, or crypto). If the user input does not exist, it raises a KeyError to notify the user.
 
@@ -195,148 +93,7 @@ class Portofolio:
         else:
             raise KeyError('position_id does not exist.')
 
-    def decrease_stock_amount_memory(self, stock_name: str, amount: int):
-        """This function is called from close_position function if the position exists, and should not be directly called by the user because of data integrity. This function subtracts the position amount from the stock asset (self.stocks) and if it reaches 0 then completely deletes the asset.
-
-        Args:
-            stock_name (str): The name of the stock of type string ("Apple")
-            amount (int): The amount of the stock position at time of purchase
-
-        Raises:
-            KeyError: Stock name does not exist.
-        """
-        if stock_name in self.stocks_df['stock_name'].values:
-            self.stocks_df.loc[self.stocks_df['stock_name'] == stock_name, 'total_amount'] -= amount          
-            self.decrease_stock_amount_db(stock_name, amount)
-
-            new_amount = self.stocks_df.loc[self.stocks_df['stock_name'] == stock_name, 'total_amount'].values[0]
-            if new_amount == 0:
-                self.stocks_df.drop(self.stocks_df[self.stocks_df['stock_name'] == stock_name].index, inplace=True)
-                print("Stock asset ", stock_name, " reached total amount 0 and was removed from memory")
-        else:
-            raise KeyError('Stock name ', stock_name, ' does not exist.')
-
-    def decrease_etf_amount_memory(self, etf_name: str, amount: int):
-        """This function is called from close_position function if the position exists, and should not be directly called by the user because of data integrity. This function subtracts the position amount from the ETF asset (self.etfs) and if it reaches 0 then completely deletes the asset.
-
-        Args:
-            etf_name (str): The name of the ETF of type string ("VOO")
-            amount (int): The amount of the ETF position at time of purchase
-
-        Raises:
-            KeyError: ETF name does not exist
-        """
-        if etf_name in self.etfs_df['etf_name'].values:
-            self.etfs_df.loc[self.etfs_df['etf_name'] == etf_name, 'total_amount'] -= amount
-            self.decrease_etf_amount_db(etf_name, amount)
-
-            new_amount = self.etfs_df.loc[self.etfs_df['etf_name'] == etf_name, 'total_amount'].values[0]
-            if new_amount == 0:
-                self.etfs_df.drop(self.etfs_df[self.etfs_df['etf_name'] == etf_name].index, inplace=True)
-                print("ETF asset ", etf_name, " reached total amount 0 and was removed from memory")
-        else:
-            raise KeyError('ETF name ', etf_name, ' does not exist.')
-
-    def decrease_crypto_amount_memory(self, crypto_name: str, amount: int):
-        """This function is called from close_position function if the position exists, and should not be directly called by the user because of data integrity. This function subtracts the position amount from the crypto asset (self.crypto) and if it reaches 0 then completely deletes the asset.
-
-        Args:
-            crypto_name (str): The name of the crypto of type string ("Bitcoin")
-            amount (int): The amount of the crypto position at time of purchase
-
-        Raises:
-            KeyError: Crypto name does not exist
-        """
-        if crypto_name in self.crypto_df['crypto_name'].values:
-            self.crypto_df.loc[self.crypto_df['crypto_name'] == crypto_name, 'total_amount'] -= amount
-            self.decrease_crypto_amount_db(crypto_name, amount)
-
-            new_amount = self.crypto_df.loc[self.crypto_df['crypto_name'] == crypto_name, 'total_amount'].values[0]
-            if new_amount == 0:
-                self.crypto_df.drop(self.crypto_df[self.crypto_df['crypto_name'] == crypto_name].index, inplace=True)
-                print("Crypto asset ", crypto_name, " reached total amount 0 and was removed from memory")
-        else:
-            raise KeyError('Crypto name ', crypto_name, ' does not exist.')
     
-    def delete_position_from_memory(self, position_id: str):
-        """Function that deletes a position from memory (self.positions) by using the position_id string parameter to search for it. Here we do not have to implement any error checking because it has
-        already been checked that the position exists, so coming so far means that the position exist in self.positions or else an error would have been raised sooner.
-
-        Args:
-            position_id (str): The string position_id to look for ("J444")
-        """
-        to_drop = self.positions_df[self.positions_df['position_id'] == position_id].index
-        self.positions_df.drop(to_drop, inplace=True)
-        print('Position with id ', position_id, ' was deleted from memory.')
-        self.delete_position_db(position_id)
-
-    def generate_unique_id(self):
-        flag = False
-        while flag == False:
-            letter = random.choice(string.ascii_uppercase)
-            numbers = f"{random.randint(0, 999):03d}"
-            unique_id = f"{letter}{numbers}"
-            if unique_id not in self.unique_codes:
-                self.unique_codes.add(unique_id)
-                flag = True
-                return unique_id
-    
-    def load_data_from_db_to_memory(self):
-        with self.engine.connect() as connection:
-            self.stocks_df = pd.read_sql_table('stocks', connection)
-            self.etfs_df = pd.read_sql_table('etfs', connection)
-            self.crypto_df = pd.read_sql_table('crypto', connection)
-            self.positions_df = pd.read_sql_table('positions', connection)
-            self.unique_codes = set(self.positions_df['position_id'])
-            self.show_all_assets_in_memory()
-
-    def show_all_assets_in_memory(self):
-        print("Stocks")
-        print(self.stocks_df)
-        print()
-        
-        print("ETFs")
-        print(self.etfs_df)
-        print()
-        
-        print("Crypto")
-        print(self.crypto_df)
-        print()
-        
-        print("Positions")
-        print(self.positions_df)
-
-    def upload_tables_to_db(self):
-        self.stocks_df.to_sql('stocks', con=self.engine, if_exists='replace', index=False)
-        self.etfs_df.to_sql('etfs', con=self.engine, if_exists='replace', index=False)
-        self.crypto_df.to_sql('crypto', con=self.engine, if_exists='replace', index=False)
-        self.positions_df.to_sql('positions', con=self.engine, if_exists='replace', index=False)
-   
-    def create_empty(self):
-        queries = [
-            """CREATE TABLE IF NOT EXISTS stocks (
-                stock_name VARCHAR(50) NOT NULL PRIMARY KEY,
-                total_amount INT NOT NULL,
-                sector VARCHAR(50) NOT NULL
-            );""",
-            """CREATE TABLE IF NOT EXISTS etfs (
-                etf_name VARCHAR(50) NOT NULL PRIMARY KEY,
-                total_amount INT NOT NULL
-            );""",
-            """CREATE TABLE IF NOT EXISTS crypto (
-                crypto_name VARCHAR(50) NOT NULL PRIMARY KEY,
-                total_amount INT NOT NULL
-            );""",
-            """CREATE TABLE IF NOT EXISTS positions (
-                position_id VARCHAR(50) PRIMARY KEY,
-                position_name VARCHAR(50) NOT NULL,
-                position_amount INT NOT NULL,
-                asset_type VARCHAR(50) NOT NULL,
-                sector VARCHAR(50) NOT NULL
-            );"""
-        ]
-        for query in queries:
-            self.execute_query(query)
 
     def append_position_to_db(self, position_id: str, asset_name: str, amount: int, asset_type: str, sector: str):
         query = """
@@ -414,46 +171,6 @@ class Portofolio:
         total_assets_df = pd.DataFrame(data)
         print(total_assets_df)
 
-    def decrease_stock_amount_db(self, asset_name: str, amount: int):
-        query = """
-        UPDATE stocks 
-        SET total_amount = total_amount - :amount 
-        WHERE stock_name = :name;
-
-        DELETE FROM stocks 
-        WHERE stock_name = :name AND total_amount <= 0;
-        """
-        params = {'amount': amount, 'name': asset_name}
-        self.execute_query(query, params)
-        print('Stock amount decreased')
-
-    def decrease_etf_amount_db(self, asset_name: str, amount: int):
-        query = """
-        UPDATE etfs 
-        SET total_amount = total_amount - :amount 
-        WHERE etf_name = :name;
-
-        DELETE FROM etfs 
-        WHERE etf_name = :name AND total_amount <= 0;
-        """
-        params = {'amount': amount, 'name': asset_name}
-        self.execute_query(query, params)
-        print('ETF amount decreased')
-
-    def decrease_crypto_amount_db(self, asset_name: str, amount: int):
-        query = """
-        UPDATE crypto 
-        SET total_amount = total_amount - :amount 
-        WHERE crypto_name = :name;
-
-        DELETE FROM crypto
-        WHERE crypto_name = :name AND total_amount <= 0;
-        """
-        params = {'amount': amount, 'name': asset_name}
-        self.execute_query(query, params)
-        print('Crypto amount decreased')
-
-    
 
     def remove_asset(self, asset_name: str):
         position_id_list = []
@@ -479,7 +196,7 @@ class Portofolio:
         self.create_empty()
         print("All tables in memory and database have been reset")
 
-    def compare_tables_equality(self, asset_table: str):
+    
         memory_table_df = getattr(self, f"{asset_table}_df", None) # table to be compared is set dynamically depending on user input
         if memory_table_df is None:
             print("Not a valid table name")
@@ -514,6 +231,7 @@ class Portofolio:
 
 
 master = Portofolio("Raul")
+
 
 
 
