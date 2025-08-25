8763a1535eb4778dca5a5f7cce1ef63487ec5969
ElDiablo247 <raul_birta10@yahoo.com>
2025-08-25 22:19:31 +0200

    Extended the positions table with attributes (open_price, asset_share, sector, open_datetime). Created new table called transactions to store previous transactions, so like a table of historical positions that have been closed. Merged user_id_generator and position_id_generator functions into one function. Function register_user doesnt use the username anymore to create the id but just a random id is generated. Open position function now does some checks to see if some inputs are valid and after that uses the new function get_asset_data to retrieve critical info from the asset that we want to buy. After the data is retrieved it is used to insert a new position into the DB by running a querry. The new function itself uses the yfinance library (API) to retrieve data like market state (Open, close), asset current price, asset type, sector in which the asset operates and then returns a list with the results. A new function calculate_asset_shares computes the amount of shares bought depending on current price of the asset and the amount the investor is investinng. NEXT TIME, WE WORK ON close_position function AND ALSO UPDATE THE DOCUMENTATION FOR ALL THE CHANGED FUNCTIONS.

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime
 
 
-ticker_symbol = yf.Ticker("AAPL")
+ticker_symbol = yf.Ticker("SOL-USD")
 data = ticker_symbol.info
 market_state = data.get('marketState')
 print(market_state)
@@ -11,3 +11,4 @@ price = data.get('regularMarketPrice')
 unix_timestamp = data.get('regularMarketTime')
 trade_time = datetime.fromtimestamp(unix_timestamp)
 print(price, trade_time)
+
